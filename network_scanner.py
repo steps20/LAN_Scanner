353de705a2f0139de87ba81adf01b6566804daa8
steps20 <71916476+steps20@users.noreply.github.com>
2023-04-21 12:09:09 -0700

    Update network_scanner.py

--- network_scanner.py
+++ network_scanner.py
@@ -1,13 +1,12 @@
-#!/usr.bin.env python
+import argparse
 import scapy.all as scapy
-import optparse
 
 def scan(ip):
     arp_request = scapy.ARP(pdst=ip)
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
     arp_request_broadcast = broadcast/arp_request
-    answered = scapy.srp(arp_request_broadcast, timeout=1)[0]
-    
+    answered = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
     clients_list = []
     for item in answered:
         client_dict = {"ip": item[1].psrc, "mac": item[1].hwsrc}
@@ -21,16 +20,13 @@ def print_result(result_list):
         print(client["ip"]+"\t\t"+client["mac"])
 
 def get_arguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-t", "--target", dest="ip", help="Enter the desired ip range to scan")
-    (options, arguments) = parser.parse_args()
-    if not options.ip:
-        print("Please enter an ip address using '-t'")
-        exit()
-    else:
-        return options
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t", "--target", dest="ip", help="Enter the desired ip range to scan")
+    args = parser.parse_args()
+    if not args.ip:
+        parser.error("Please enter an IP address range to scan using '-t'")
+    return args
 
-
-options = get_arguments()
-scan_result = scan(options.ip)
+args = get_arguments()
+scan_result = scan(args.ip)
 print_result(scan_result)
